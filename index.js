3f7229a50e1761aae1962b557e98d5673d6b353f
Nafees Nehar <nafees87n@gmail.com>
2021-07-09 01:54:17 +0530

    refactor code, removed selenium dependency

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const { By, until } = require("selenium-webdriver");
+const CONSTANTS = require(path.join(__dirname, "CONSTANTS"));
 
 /**
  * Reads and encodes file into base64
@@ -13,21 +13,27 @@ function encodeFile(file) {
 }
 
 /**
- * Returns base64 encoded Requestly extension which can be added to selenium-chrome
+ * Returns base64 encoded Requestly extension which can be added to selenium browser
  * @returns Encoded Extension
  */
 function getRequestlyExtension() {
   return encodeFile(
-    path.join(__dirname, "extensions", "requestly-21.6.30.crx")
+    path.join(
+      __dirname,
+      CONSTANTS.EXTENSION_FOLDER,
+      CONSTANTS.REQUESTLY_EXTENSION
+    )
   );
 }
 
 /**
  * Imports shared-list to user rules in requestly
- * @param {Object} driver selenium webDriver instance
+ * @param {Object} driver selenium-webdriver instance
+ * @param {Object} until selenium-webdriver until instance to wait for a condition
+ * @param {Object} By selenium-webdriver By instance to locate an element
  * @param {String} sharedListUrl Requestly SharedList Url to be imported
  */
-const importRequestlySharedList = async (driver, sharedListUrl) => {
+const importRequestlySharedList = async (driver, until, By, sharedListUrl) => {
   const tabs = await driver.getAllWindowHandles();
   // Switch to /rules which gets opened after installation of extension because
   // The focus is by default on the 1st tab opened.
@@ -38,11 +44,13 @@ const importRequestlySharedList = async (driver, sharedListUrl) => {
   await driver.switchTo().window(tabs[1]);
   // Open Page Url
   await driver.get(sharedListUrl);
-
   // Wait until Import List button gets loaded
-  await driver.wait(until.elementsLocated(By.css(".btn-primary")), 10000);
+  await driver.wait(
+    until.elementsLocated(By.css(CONSTANTS.IMPORT_LIST_BUTTON_CLASS)),
+    10000
+  );
   // Click on import List button
-  await driver.findElement(By.css(".btn-primary")).click();
+  await driver.findElement(By.css(CONSTANTS.IMPORT_LIST_BUTTON_CLASS)).click();
 };
 
 module.exports = {

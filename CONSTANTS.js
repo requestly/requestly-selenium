73042e1722865c986d4c4d684213e5dfa49a54d2
Nafees Nehar <nafees87n@gmail.com>
2021-07-10 00:25:17 +0530

    updated constants

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -1,5 +1,10 @@
 module.exports = {
   EXTENSION_FOLDER: "extensions",
-  REQUESTLY_EXTENSION: "requestly_extension.crx",
+  REQUESTLY_FIREFOX_EXTENSION: "requestly_extension.xpi",
+  REQUESTLY_CHROME_EXTENSION: "requestly_extension.crx",
   IMPORT_LIST_BUTTON_CLASS: "button.btn-icon.btn-3.btn.btn-primary",
+  BROWSERS: {
+    FIREFOX: "firefox",
+    CHROME: "chrome",
+  },
 };

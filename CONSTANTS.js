eb73f4ab2ac049ab62a7e294d359cbecedb79e43
Nafees Nehar <nafees87n@gmail.com>
2021-12-29 21:05:54 +0530

    fix: click on import button

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -2,7 +2,7 @@ module.exports = {
   EXTENSION_FOLDER: "extensions",
   REQUESTLY_FIREFOX_EXTENSION: "requestly_extension.xpi",
   REQUESTLY_CHROMIUM_EXTENSION: "requestly_extension.crx",
-  IMPORT_LIST_BUTTON_CLASS: "button.btn-icon.btn-3.btn.btn-primary",
+  IMPORT_LIST_BUTTON_CLASS: "button.ant-btn.ant-btn-primary",
   BROWSERS: {
     FIREFOX: "firefox",
     CHROME: "chrome",
